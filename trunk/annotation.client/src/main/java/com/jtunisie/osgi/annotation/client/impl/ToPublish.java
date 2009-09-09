a5c9a476bf035e7e27534d58a27c52733fda1551
ouertani <ouertani@48ea6d74-9d63-11de-8bf8-05dd22f30006>
2009-09-09 23:15:27 +0000

    [SO] change names

--- trunk/annotation.client/src/main/java/com/jtunisie/osgi/annotation/client/impl/ToPublish.java
+++ trunk/annotation.client/src/main/java/com/jtunisie/osgi/annotation/client/impl/ToPublish.java
@@ -1,6 +1,6 @@
 package com.jtunisie.osgi.annotation.client.impl;
 
-import com.jtunisie.osgi.annotation.Publish;
+import com.jtunisie.osgi.annotation.RegisterService;
 import com.jtunisie.osgi.annotation.client.IService;
 import com.jtunisie.osgi.annotation.NamedInterface;
 import com.jtunisie.osgi.annotation.Prop;
@@ -9,7 +9,7 @@ import com.jtunisie.osgi.annotation.Prop;
  *
  * @author slim ouertani
  */
-@Publish(id = "tunisie",
+@RegisterService(id = "tunisie",
 NamedInterfaces = {@NamedInterface(IService.class)},
     Props = {
     @Prop(key = "url", value = "/jtunisie"),
